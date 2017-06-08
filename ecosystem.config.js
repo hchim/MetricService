68036e168916cb3d97ca05ffaecb30292511a44a
Hui Chen <huiche@amazon.com>
2017-06-08 00:46:56 -0700

    Set default env to production.

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,23 +4,23 @@ module.exports = {
    * http://pm2.keymetrics.io/docs/usage/application-declaration/
    */
   apps : [
-    {
-      name: "MetricService",
-      script: "./bin/www",
-      env: {
-        NODE_ENV: "production"
-      },
-      env_development : {
-        NODE_ENV: "development"
-      },
-      watch: false,
-      instances: 1,
-      exec_mode: "cluster",
-      log_date_format: "YYYY-MM-DD HH:mm Z",
-      error_file: "./log/errors.log",
-      out_file: "./log/out.log",
-      combine_logs: true,
-      merge_logs: true
-    }
+      {
+          name: "MetricService",
+          script: "./bin/www",
+          env: {
+              NODE_ENV: "production"
+          },
+          env_development : {
+              NODE_ENV: "development"
+          },
+          watch: false,
+          instances: 1,
+          exec_mode: "cluster",
+          log_date_format: "YYYY-MM-DD HH:mm Z",
+          error_file: "./log/errors.log",
+          out_file: "./log/out.log",
+          combine_logs: true,
+          merge_logs: true
+      }
   ]
 }
