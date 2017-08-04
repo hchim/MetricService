417f62e76b5cc794f21ef7ceac10fbbcb4f8f99e
Hui Chen <huiche@amazon.com>
2017-08-03 21:38:50 -0700

    remove access log rotator

--- app.js
+++ app.js
@@ -1,8 +1,5 @@
 var express = require('express');
-var path = require('path');
-var morgan = require('morgan');
 var bodyParser = require('body-parser');
-var FileStreamRotator = require('file-stream-rotator');
 var fs = require('fs');
 var conf = require("./config");
 var middlewares = require('service-middlewares')(conf)
@@ -15,22 +12,9 @@ var index = require('./routes/index');
 var metrics = require('./routes/metrics');
 
 var app = express();
-
-if (conf.get("env") === "production") {
-    var logDirectory = __dirname + '/log';
-    // ensure log directory exists
-    fs.existsSync(logDirectory) || fs.mkdirSync(logDirectory);
-    // create a rotating write stream
-    var accessLogStream = FileStreamRotator.getStream({
-        date_format: conf.get('log.dateformat'),
-        filename: logDirectory + '/access-%DATE%.log',
-        frequency: conf.get("log.frequency"),
-        verbose: false
-    });
-    app.use(morgan('combined', {stream: accessLogStream}));
-} else {
-    app.use(morgan('dev'));
-}
+var logDirectory = __dirname + '/log';
+// ensure log directory exists
+fs.existsSync(logDirectory) || fs.mkdirSync(logDirectory);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
