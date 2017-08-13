6f9c2e631a51674ff3cf6976554af4bf19f188d5
Hui Chen <huiche@amazon.com>
2017-08-13 02:42:30 -0700

    Update service handlers

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ var fs = require('fs');
 var conf = require("./config");
 var middlewares = require('service-middlewares')(conf)
 var utils = require('servicecommonutils');
-
 var winston = utils.getWinston(conf.get('env'));
 
 //routes
@@ -13,50 +12,28 @@ var metrics = require('./routes/metrics');
 
 var app = express();
 var logDirectory = __dirname + '/log';
-// ensure log directory exists
 fs.existsSync(logDirectory) || fs.mkdirSync(logDirectory);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use('/', index)
-//request signature checkup
 if (conf.get("env") !== 'test') {
     app.use(middlewares.signature_middleware)
 }
-
-// setup routes
 app.use('/metrics', metrics);
 
-//catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    var err = new Error('404 Not Found');
-    err.status = 404;
-    next(err);
-});
-
-// error handlers
-
-// development error handler
-// will print stack trace
-if (conf.get("env") === 'development') {
+app.use(middlewares.error_404_middleware);
+if (conf.get("env") !== 'production') {
+    app.use(middlewares.error_500_middleware_dev);
+} else {
     app.use(function(err, req, res, next) {
         res.status(err.status || 500);
+        winston.log('error', err.message, err);
         res.json(utils.encodeResponseBody(req, {
-            message: err.message,
-            error: err
+            message: err.message
         }));
     });
 }
-
-// production error handler
-// no stack traces leaked to user
-app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    winston.log('error', err.message, err);
-    res.json(utils.encodeResponseBody(req, {
-        message: err.message
-    }));
-});
 
 module.exports = app;
